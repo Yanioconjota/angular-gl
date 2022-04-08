4f59f2b5ce29cfd13fb3f407893353b0f2bce46b
Janio Isacura <janioisacura@gmail.com>
2022-04-08 13:35:38 -0300

    component location changed

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador.component';
+import { ContadorComponent } from './contador/contador.component';
 
 @NgModule({
   declarations: [
