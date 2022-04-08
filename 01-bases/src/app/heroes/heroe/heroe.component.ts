01e976ab46cdf63155fe477e7bad4985290a15c3
Janio Isacura <janioisacura@gmail.com>
2022-04-08 14:57:32 -0300

    Change hero method added

--- 01-bases/src/app/heroes/heroe/heroe.component.ts
+++ 01-bases/src/app/heroes/heroe/heroe.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Heroe {
+  nombre: string;
+  edad: number;
+}
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
@@ -8,15 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent {
 
+  value = 0;
   nombre: string = 'Ironman';
   edad: number = 45;
 
-  get nombreCapitalizado() {
+  
+
+  heroes: Heroe[] = [
+    {nombre: 'Ironman', edad: 45},
+    {nombre: 'Captain America', edad: 75},
+    {nombre: 'Spider-man', edad: 17}
+  ];
+
+  get nombreCapitalizado():string {
     return this.nombre.toUpperCase();
   }
 
   obtenerNombre ():string {
     return `${this.nombre} - ${this.edad}`;
+  }
+
+  cambiarHeroe():void {
+   
+   if (this.value === (this.heroes.length - 1)) {
+     this.value = 0;
+   } else {
+    this.value = this.value + 1
+  }
+  this.nombre = this.heroes[this.value].nombre;
+  this.edad = this.heroes[this.value].edad;
+   
   }
 
 }
