2cbc677e5eabd44fa0a8380578c8ccaeaf82b273
Janio Isacura <janioisacura@gmail.com>
2022-04-08 13:34:05 -0300

    new component added

---
+++ 01-bases/src/app/contador.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrls: ['./contador.component.scss']
+})
+
+export class ContadorComponent {
+  numero: number = 10;
+
+  base: number = 10;
+
+  acumular (valor: number) { this.numero += valor };
+
+  reset () { 
+    this.base = 10;
+    this.numero = this.base;
+  }
+}
