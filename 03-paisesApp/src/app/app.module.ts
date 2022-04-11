8f2f89f9fe2e4c5f940c6af23a660a3fb3f0cac6
Janio Isacura <janioisacura@gmail.com>
2022-04-11 12:21:29 -0300

    Routing module and routes created

--- 03-paisesApp/src/app/app.module.ts
+++ 03-paisesApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 //Modules
+import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { PaisesModule } from './paises/paises.module';
 //Components
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
     PaisesModule
   ],
