b13b45f42752c65156e57742dda13cbda2c4acbf
Janio Isacura <janioisacura@gmail.com>
2022-04-11 15:43:22 -0300

    por-pais component error handling in progress

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -10,14 +10,23 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent {
 
   termino: string = '';
+  hayError: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar() {
+    this.hayError = false;
     this.paisService.buscarPais(this.termino)
-        .subscribe(resp => {
-          console.log(resp);
-        })
+        .subscribe({
+          next: (resp) => {
+            console.log(resp);
+          },
+          error: (err: any) => {
+            console.log('Error');
+            console.info(err);
+            this.hayError = true;
+          }
+        });
   }
 
 }
