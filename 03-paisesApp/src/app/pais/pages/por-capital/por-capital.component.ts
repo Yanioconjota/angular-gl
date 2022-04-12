40b61a90894af71a1ba95e5c5812e2a8a93fc646
Janio Isacura <janioisacura@gmail.com>
2022-04-12 13:57:47 -0300

    loader added to por-pais and por-capital components

--- 03-paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-capital/por-capital.component.ts
@@ -14,19 +14,23 @@ export class PorCapitalComponent {
   hayError: boolean = false;
   paises: Pais[] = [];
   placeholder: string = '';
+  loading: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string) {
+    this.loading = true;
     this.hayError = false;
     this.termino = termino;
     this.paisService.buscarCapital(this.termino)
         .subscribe({
           next: (paises: Pais[]) => {
+            this.loading = false;
             console.log(paises);
             this.paises = paises;
           },
           error: (err: any) => {
+            this.loading = false;
             console.log('Error');
             console.info(err);
             this.hayError = true;
@@ -38,6 +42,7 @@ export class PorCapitalComponent {
   sugerencias(termino: string) {
     //Elimina el error cuando empezamos a escribir
     this.hayError = false;
+    this.loading = false;
     console.log('sugerencias: ', termino);
   }
 
--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -14,19 +14,23 @@ export class PorPaisComponent {
   hayError: boolean = false;
   paises: Pais[] = [];
   placeholder: string = '';
+  loading: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string) {
+    this.loading = true;
     this.hayError = false;
     this.termino = termino;
     this.paisService.buscarPais(this.termino)
         .subscribe({
           next: (paises: Pais[]) => {
+            this.loading = false;
             console.log(paises);
             this.paises = paises;
           },
           error: (err: any) => {
+            this.loading = false;
             console.log('Error');
             console.info(err);
             this.hayError = true;
@@ -38,6 +42,7 @@ export class PorPaisComponent {
   sugerencias(termino: string) {
     //Elimina el error cuando empezamos a escribir
     this.hayError = false;
+    this.loading = false;
     console.log('sugerencias: ', termino);
   }
 
