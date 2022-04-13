50479f8692b7445e82df8ef699e7fa7610420154
Janio Isacura <janioisacura@gmail.com>
2022-04-13 11:15:40 -0300

    sugerencias method return countries suggestions

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -13,14 +13,18 @@ export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Pais[] = [];
+  paisesSugeridos: Pais[] = [];
   placeholder: string = '';
   loading: boolean = false;
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) { }
 
   buscar(termino: string) {
+    if (termino === '') { return }
     this.loading = true;
     this.hayError = false;
+    this.mostrarSugerencias = false;
     this.termino = termino;
     this.paisService.buscarPais(this.termino)
         .subscribe({
@@ -40,9 +44,27 @@ export class PorPaisComponent {
   }
 
   sugerencias(termino: string) {
+    if (termino === '') { return }
     //Elimina el error cuando empezamos a escribir
     this.hayError = false;
     this.loading = false;
+    this.mostrarSugerencias = true;
+    this.termino = termino;
+    this.paisService.buscarPais(termino)
+        .subscribe({
+          next: (paises: Pais[]) => {
+            console.log(paises);
+            this.paisesSugeridos = paises.splice(0,5);
+          },
+          error: (err: any) => {
+            this.paisesSugeridos = [];
+            //console.log(err);
+          }
+        })
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
   }
 
 }
