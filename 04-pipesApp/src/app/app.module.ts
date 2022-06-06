d6b3ccf4269929b53679324a184682e0898281e4
Janio Isacura <janioisacura@gmail.com>
2022-06-06 11:56:28 -0300

    Setting up PrimeNG

--- 04-pipesApp/src/app/app.module.ts
+++ 04-pipesApp/src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+
+//PrimeNG
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    CommonModule
+    BrowserAnimationsModule,
+    CommonModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
